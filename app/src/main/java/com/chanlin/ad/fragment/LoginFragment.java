4944030c867c4a065694d88b6d8fbdee63f621b5
Gang Ma <mg.abcd@gmail.com>
2020-09-21 09:52:24 +0800

    增加登录页面

---
+++ app/src/main/java/com/chanlin/ad/fragment/LoginFragment.java
@@ -0,0 +1,147 @@
+package com.chanlin.ad.fragment;
+
+import android.app.Dialog;
+import android.app.ProgressDialog;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import com.chanlin.ad.R;
+import com.chanlin.ad.base.BaseFragment;
+import com.chanlin.ad.config.PushConfig;
+import com.chanlin.ad.data.User;
+import com.qmuiteam.qmui.widget.QMUITopBarLayout;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+import cn.leancloud.AVUser;
+import io.reactivex.Observer;
+import io.reactivex.disposables.Disposable;
+
+public class LoginFragment extends BaseFragment {
+    @BindView(R.id.topbar)
+    QMUITopBarLayout mTopBar;
+
+    @BindView(R.id.login_phone)
+    EditText mPhoneField;
+
+    @BindView(R.id.login_password)
+    EditText mPasswordField;
+
+    @BindView(R.id.login_submit)
+    Button mSubmitButton;
+
+    public static final String TAG = "LoginFragment";
+    private String mPassword;
+    private String mPhone;
+    private Dialog mProgressDialog;
+    private PushConfig mPush;
+
+    @Override
+    protected View onCreateView() {
+        mPush = PushConfig.get(getActivity());
+        View root = LayoutInflater.from(getActivity()).inflate(R.layout.fragment_login, null);
+        ButterKnife.bind(this, root);
+
+        initTopBar();
+        initView();
+
+        return root;
+    }
+
+    private void initTopBar() {
+        mTopBar.addLeftBackImageButton().setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                popBackStack();
+            }
+        });
+
+        mTopBar.setTitle("登录");
+    }
+
+    private void initView() {
+        mPhoneField.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence c, int start, int before, int count) {
+                mPhone = c.toString();
+            }
+
+            public void beforeTextChanged(CharSequence c, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable c) {
+            }
+        });
+
+        String lastLoginPhone = mPush.getPhone();
+        if (lastLoginPhone != null && !lastLoginPhone.equals("")) {
+            mPhoneField.setText(lastLoginPhone);
+            mPhone = lastLoginPhone;
+        }
+
+        mPasswordField.addTextChangedListener(new TextWatcher() {
+            public void onTextChanged(CharSequence c, int start, int before, int count) {
+                mPassword = c.toString();
+            }
+
+            public void beforeTextChanged(CharSequence c, int start, int count, int after) {
+            }
+
+            public void afterTextChanged(Editable c) {
+            }
+        });
+
+        mSubmitButton.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+
+                if (mPhone == null || mPhone.equals("")) {
+                    Toast.makeText(getActivity(), "手机号码不能为空！", Toast.LENGTH_SHORT).show();
+                } else if (mPhone.length() > 11) {
+                    Toast.makeText(getActivity(), "手机号码不能多于11位！", Toast.LENGTH_SHORT).show();
+                } else if (mPassword == null || mPassword.equals("")) {
+                    Toast.makeText(getActivity(), "密码不能为空！", Toast.LENGTH_SHORT).show();
+                } else if (mPassword.length() > 30) {
+                    Toast.makeText(getActivity(), "密码不能多于30个字符！", Toast.LENGTH_SHORT).show();
+                } else {
+
+                    mProgressDialog =
+                            ProgressDialog.show(getActivity(), "", "正在登陆，请稍候...", true);
+                    AVUser.loginByMobilePhoneNumber(mPhone, mPassword).subscribe(new Observer<AVUser>() {
+                        public void onSubscribe(Disposable disposable) {}
+                        public void onNext(AVUser user) {
+                            // 登录成功
+                            mProgressDialog.dismiss();
+                            mPush.setPhone(mPhone);
+                            mPush.setPassword(mPassword);
+                            User.updateLocalParams(getActivity());
+                            Log.d(TAG, "User login successfully.");
+                            Toast.makeText(getActivity(), "登陆成功", Toast.LENGTH_SHORT).show();
+                        }
+                        public void onError(Throwable throwable) {
+                            // 登录失败（可能是密码错误）
+                            String msg = throwable.getMessage().toLowerCase();
+                            if (msg.equals("mobile phone number isn't verified.")) {
+                                mPush.setVerifiedPhone(mPhone.trim());
+                                AVUser.requestMobilePhoneVerifyInBackground(mPhone.trim()).blockingSubscribe();
+                                Toast.makeText(getActivity(), "请先验证该手机号码！", Toast.LENGTH_SHORT).show();
+//                                Intent i = new Intent(getActivity(), PhoneVerifyActivity.class);
+//                                startActivity(i);
+                            } else {
+                                Toast.makeText(getActivity(), "手机号码或者密码不正确！", Toast.LENGTH_SHORT).show();
+                                Log.e(TAG, "User login failed:" + throwable.getMessage());
+                            }
+                        }
+                        public void onComplete() {}
+                    });
+
+
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/chanlin/ad/view/home/HomeInfoView.java
+++ app/src/main/java/com/chanlin/ad/view/home/HomeInfoView.java
@@ -1,32 +1,22 @@
 package com.chanlin.ad.view.home;
 
-import android.annotation.TargetApi;
 import android.content.Context;
-import android.os.AsyncTask;
-import android.os.Parcelable;
-import android.util.SparseArray;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Toast;
 
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
-
 import com.chanlin.ad.R;
 import com.chanlin.ad.base.BaseFragment;
-import com.chanlin.ad.base.BaseRecyclerAdapter;
-import com.chanlin.ad.base.RecyclerViewHolder;
-import com.chanlin.ad.data.Trade;
-import com.chanlin.ad.data.TradeLab;
+import com.chanlin.ad.config.PushConfig;
+import com.chanlin.ad.fragment.LoginFragment;
 import com.chanlin.ad.fragment.QDAboutFragment;
 import com.chanlin.ad.listener.HomeViewListener;
-import com.qmuiteam.qmui.util.QMUIViewHelper;
+import com.qmuiteam.qmui.util.QMUIDisplayHelper;
 import com.qmuiteam.qmui.widget.QMUITopBarLayout;
 import com.qmuiteam.qmui.widget.QMUIWindowInsetLayout;
-import com.qmuiteam.qmui.widget.pullRefreshLayout.QMUIPullRefreshLayout;
-
-import java.util.List;
+import com.qmuiteam.qmui.widget.grouplist.QMUICommonListItemView;
+import com.qmuiteam.qmui.widget.grouplist.QMUIGroupListView;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -40,55 +30,21 @@ public class HomeInfoView extends QMUIWindowInsetLayout {
     @BindView(R.id.topbar)
     QMUITopBarLayout mTopBar;
 
-    @BindView(R.id.recyclerView)
-    RecyclerView mRecyclerView;
-
-    @BindView(R.id.pull_to_refresh)
-    QMUIPullRefreshLayout mPullRefreshLayout;
+    @BindView(R.id.groupListView)
+    QMUIGroupListView mGroupListView;
 
     private HomeViewListener mHomeViewListener;
-    private int mDiffRecyclerViewSaveStateId = QMUIViewHelper.generateViewId();
-    private BaseRecyclerAdapter<Trade> mAdapter;
-    private List<Trade> mTrades;
+    private Context mContext;
+    private PushConfig mPush;
 
     public HomeInfoView(Context context) {
         super(context);
-        LayoutInflater.from(context).inflate(R.layout.home_layout, this);
+        mContext = context;
+        mPush = PushConfig.get(mContext);
+        LayoutInflater.from(context).inflate(R.layout.info_layout, this);
         ButterKnife.bind(this);
         initTopBar();
-        initData();
-    }
-
-    @TargetApi(11)
-    private class RemoteDataTask extends AsyncTask<Void, Void, Void> {
-        @Override
-        protected Void doInBackground(Void... params) {
-            mTrades = TradeLab.get(getContext()).findTrades();
-            return null;
-        }
-
-        @Override
-        protected void onPreExecute() {
-            super.onPreExecute();
-        }
-
-        @Override
-        protected void onProgressUpdate(Void... values) {
-            super.onProgressUpdate(values);
-        }
-
-        @Override
-        protected void onPostExecute(Void result) {
-            loadData();
-        }
-    }
-
-    private void fetchData () {
-        new RemoteDataTask().execute();
-    }
-
-    private void loadData() {
-        mAdapter.setData(mTrades);
+        initGroupListView();
     }
 
     protected void startFragment(BaseFragment fragment) {
@@ -102,7 +58,7 @@ public class HomeInfoView extends QMUIWindowInsetLayout {
     }
 
     private void initTopBar() {
-        mTopBar.setTitle("我");
+        mTopBar.setTitle("账户");
 
         mTopBar.addRightImageButton(R.mipmap.icon_topbar_about, R.id.topbar_right_about_button).setOnClickListener(new OnClickListener() {
             @Override
@@ -113,71 +69,40 @@ public class HomeInfoView extends QMUIWindowInsetLayout {
         });
     }
 
-    private void initData() {
-        mRecyclerView.setLayoutManager(new LinearLayoutManager(getContext()) {
-            @Override
-            public RecyclerView.LayoutParams generateDefaultLayoutParams() {
-                return new RecyclerView.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
-                        ViewGroup.LayoutParams.WRAP_CONTENT);
-            }
-        });
+    private void initGroupListView() {
+        QMUICommonListItemView loginItem = mGroupListView.createItemView("登录");
+        loginItem.setAccessoryType(QMUICommonListItemView.ACCESSORY_TYPE_CHEVRON);
 
-        mAdapter = new BaseRecyclerAdapter<Trade>(getContext(), null) {
-            @Override
-            public int getItemLayoutId(int viewType) {
-                return android.R.layout.simple_list_item_1;
-            }
+        QMUICommonListItemView accountItem = mGroupListView.createItemView("账户");
+        accountItem.setAccessoryType(QMUICommonListItemView.ACCESSORY_TYPE_CHEVRON);
 
+        View.OnClickListener onClickListener = new View.OnClickListener() {
             @Override
-            public void bindData(RecyclerViewHolder holder, int position, Trade item) {
-                holder.setText(android.R.id.text1, item.getTitle());
+            public void onClick(View v) {
+                if (v instanceof QMUICommonListItemView) {
+                    String text = ((QMUICommonListItemView) v).getText().toString();
+                    Toast.makeText(mContext, text + " is Clicked", Toast.LENGTH_SHORT).show();
+                    switch(text) {
+                        case "登录":
+                            LoginFragment loginFragment = new LoginFragment();
+                            startFragment(loginFragment);
+                            break;
+                        default:
+                            break;
+                    }
+
+                }
             }
         };
-        mAdapter.setOnItemClickListener(new BaseRecyclerAdapter.OnItemClickListener() {
-            @Override
-            public void onItemClick(View itemView, int pos) {
-                Toast.makeText(getContext(), "click position=" + pos, Toast.LENGTH_SHORT).show();
-            }
-        });
-        mRecyclerView.setAdapter(mAdapter);
-        fetchData();
-        mPullRefreshLayout.setOnPullListener(new QMUIPullRefreshLayout.OnPullListener() {
-            @Override
-            public void onMoveTarget(int offset) {
 
-            }
-
-            @Override
-            public void onMoveRefreshView(int offset) {
-
-            }
-
-            @Override
-            public void onRefresh() {
-                mPullRefreshLayout.postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        fetchData();
-                        mPullRefreshLayout.finishRefresh();
-                    }
-                }, 2000);
-            }
-        });
-    }
-
-    @Override
-    protected void dispatchSaveInstanceState(SparseArray<Parcelable> container) {
-        int id = mRecyclerView.getId();
-        mRecyclerView.setId(mDiffRecyclerViewSaveStateId);
-        super.dispatchSaveInstanceState(container);
-        mRecyclerView.setId(id);
-    }
-
-    @Override
-    protected void dispatchRestoreInstanceState(SparseArray<Parcelable> container) {
-        int id = mRecyclerView.getId();
-        mRecyclerView.setId(mDiffRecyclerViewSaveStateId);
-        super.dispatchRestoreInstanceState(container);
-        mRecyclerView.setId(id);
+        int size = QMUIDisplayHelper.dp2px(getContext(), 20);
+        QMUIGroupListView.newSection(getContext())
+                .setTitle(null)
+                .setDescription(null)
+                .setLeftIconSize(size, ViewGroup.LayoutParams.WRAP_CONTENT)
+                .addItemView(loginItem, onClickListener)
+                .addItemView(accountItem, onClickListener)
+                .setMiddleSeparatorInset(QMUIDisplayHelper.dp2px(getContext(), 16), 0)
+                .addTo(mGroupListView);
     }
 }
